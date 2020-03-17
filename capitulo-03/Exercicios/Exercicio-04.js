f18eafcca4ec54784edb067414791eb20e8f71c8
Mila <kaah.fernandess@hotmail.com>
2020-03-17 19:21:25 -0300

    :sparkles: Exercicio 05 - Feito !

--- capitulo-03/Exercicios/Exercicio-04.js
+++ capitulo-03/Exercicios/Exercicio-04.js
@@ -5,13 +5,6 @@ Exemplo: validaParenteses(')((()()())))'); → false
 Exemplo: "()()()" → true
 Exemplo: ")(" → false */
 
-const r1 = validaParenteses();
-if (typeof r1 === "string") {
-  console.log("Ela retornou uma string como esperado");
-} else if (typeof r1 === "undefined") {
-  console.error("###Não retornou uma string como esperado");
-}
-
 const r2 = validaParenteses("(");
 if (r2 === false) {
   console.log("Ela retornou false quando passamos um parenteses de ABERTURA");
@@ -40,44 +33,29 @@ if (r4 === true) {
     "###Ela não retornou true quando passamos um parenteses de ABERTURA e um parenteses de FECHAMENTO"
   );
 }
-const r5 = validaParenteses("()(");
-console.log(r5);
+
+const r5 = validaParenteses("()()()");
 
 console.log("----");
-console.log("O valor de r1 é: " + typeof r1);
-console.log("O valor de r2 é: " + r2);
-console.log("O valor de r3 é: " + r3);
-console.log("O valor de r4 é: " + r4);
+console.log("O valor de r2 é: ", r2);
+console.log("O valor de r3 é: ", r3);
+console.log("O valor de r4 é: ", r4);
 console.log("O valor de r5 é: ", r5);
 
 function validaParenteses(texto) {
-  let arrTexto;
-  if (typeof texto != "undefined") {
-    arrTexto = texto.split("");
-  } else {
-    return undefined;
-  }
-  console.log("A quantidade de parenteses é: ");
-  contaParenteses(texto);
-
-  let soma = 0;
-  for (let i = 0; i < arrTexto.length; i++) {
-    if (arrTexto[i] === "(") {
-      soma--;
+  let arrTexto = texto.split("");
+  const balanceado = !arrTexto.reduce(function(soma, texto) {
+    if (soma < 0) {
+      return soma;
     }
-    if (arrTexto[i] === ")") {
-      soma++;
+    if (texto === "(") {
+      console.log("entrou no (");
+      return ++soma;
     }
-  }
-
-  if (soma === 0) {
-    return true;
-  } else if (soma < 0 || soma >= 1) {
-    return false;
-  }
-}
-
-function contaParenteses(texto) {
-  const arrTexto = texto.split("");
-  console.log(arrTexto.length);
+    if (texto === ")") {
+      console.log(soma);
+      return --soma;
+    }
+  }, 0);
+  return balanceado;
 }
